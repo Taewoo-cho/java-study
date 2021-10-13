b834f85617f4c056018dd18db3c3d3ee93dbac55
tw <tojesus623@gmail.com>
2021-10-13 10:58:28 +0900

    2021_10_13 java-study

---
+++ HelloWorld/src/application/App.java
@@ -0,0 +1,14 @@
+package application; // 패키지 선언
+
+public class App {	// 클래스 선언
+	
+	public static void main(String[] args) {
+		// 메인 메소드에서 프로그램 시작, 코드블록 { } 안의 내용을 순서대로 실행
+		System.out.println("안녕하세요"); // 아래 콘솔에 출력하는 메소드
+		
+		System.out.println("새 자바 프로그램을");
+		
+		System.out.println("실행 합니다.");
+
+	}
+}
